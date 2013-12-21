e927609040ad9af4112fdc22e5220410c15e22e5
Atanas Korchev <akorchev@gmail.com>
2013-12-21 14:14:02 +0200

    Update the description.

--- Kendo.DynamicLinq/Properties/AssemblyInfo.cs
+++ Kendo.DynamicLinq/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kendo.DynamicLinq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Server side paging, sorting and filtering via Dynamic Linq")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Kendo.DynamicLinq")]
 [assembly: AssemblyTrademark("")]
