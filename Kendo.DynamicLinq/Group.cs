5554e3a0ad511379c526a32d215d08d127894d0b
Ali Sarkis <ali.sarkis@indiepayroll.com>
2017-01-11 16:36:26 -0500

    forgot this file

---
+++ Kendo.DynamicLinq/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Kendo.DynamicLinq
+{
+    public class Group : Sort
+    {
+        [DataMember(Name = "aggregates")]
+        public IEnumerable<Aggregator> Aggregates { get; set; }
+    }
+}
